f218f2f59da2bd93ac36633a0f91c487f7ed6e97
doomoftheworld <38981211+doomoftheworld@users.noreply.github.com>
2019-04-01 17:02:09 +0200

    version nouvelle

--- ast-nodes/Return.cpp
+++ ast-nodes/Return.cpp
@@ -7,21 +7,21 @@ string Return::buildIR(CFG * cfg){
     params.push_back(var);
     cfg->current_bb->add_IRInstr(IRInstr::ret,typeInt,params);*/
 	Type returnType = cfg->get_var_type("retValue");
-	if(returnType->getTtext() == "void"){
+	if(returnType.getText() == "void"){
 		if(expr != nullptr){
 			cerr << "Erreur dans le retour void" << endl;
 		}
 	}else{
-		if(expr != nullptr && returnType->getTtext() == expr->getType->getText()){
+		if(expr != nullptr && returnType.getText() == expr->getType().getText()){
 			string value= expr->buildIR(cfg);
 			string left = cfg->create_new_tempvar(expr->getType());
 			int offset= cfg->get_var_index("retValue");
 			vector<string>params1={left,"-"+to_string(-offset)};
-			cfg->add_IRInstr(IRInstr::ldconst,expr->getType(),params1);
+			cfg->current_bb->add_IRInstr(IRInstr::ldconst,expr->getType(),params1);
 			vector<string>params2={left,"!bp",left};
-			cfg->add_IRInstr(IRInstr::add,expr->getType(),params2);
+			cfg->current_bb->add_IRInstr(IRInstr::add,expr->getType(),params2);
 			vector<string> params3={left,value};
-			cfg->add_IRInstr(IRInstr::wmem,expr->getType(),params3);
+			cfg->current_bb->add_IRInstr(IRInstr::wmem,expr->getType(),params3);
 		}else{
 			cerr << "Erreur du type de retour ou sans expression" << endl;
 		}
